5c3048d55908d159b5025c8a1b520986d10eb7ae
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-02-26 16:46:12 +0100

    index/create

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h1>Crea un nuovo post</h1>
+  <form action="{{ route('admin.posts.store') }}" method="post">
+    @csrf
+    @method('POST')
+
+      <div class="form-group">
+        <label for="title">Titolo</label>
+        <input class="form-control" type="text" id='title'name="title" value="{{ old('title')}}">
+      </div>
+      <div class="form-group">
+        <label for="body">Testo</label>
+        <textarea class="form-control" type="text" id='body'name="body"rows="10"> {{ old('title')}}</textarea>
+      </div>
+      <input class="btn btn-primary"type="submit" name="" value="Crea">
+  </form>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// rotte pubbliche
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//rotte protette da autenticazione
+Route::prefix('admin') //prefisso rotte
+  ->namespace('Admin') //namespace (sottocartelle controller)
+  ->middleware('auth') // filtro per autenticazione
+  ->name('admin.')
+  ->group(function() {
+    // Route::get('/','HomeController@index');
+    Route::resource('posts','PostController');
+
+});
