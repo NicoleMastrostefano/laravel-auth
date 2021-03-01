5c0bace055992613ccd9a7f0a82b77584b7fe03d
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-03-01 16:57:15 +0100

    store

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::where('user_id',Auth::id())->get();
+        $posts = Post::where('user_id', Auth::id())->get();
         return view('admin.posts.index',compact('posts'));
     }
 
